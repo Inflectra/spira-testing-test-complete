3ec0ea1a5af36c52707af60e2c8beb7bc829a435
asandman <asandman@5d4b435c-4d46-d949-aab2-e601ca53af14>
2010-08-04 02:33:12 +0000

    git-svn-id: svn://doctor/SpiraTeam%20Add%20Ons/Trunk@12039 5d4b435c-4d46-d949-aab2-e601ca53af14

--- 8.0/SpiraTestCompletePlugIn/SpiraTestCompletePlugIn_i.c
+++ 8.0/SpiraTestCompletePlugIn/SpiraTestCompletePlugIn_i.c
@@ -6,7 +6,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0500 */
-/* at Mon May 25 17:10:09 2009
+/* at Tue Aug 03 22:26:22 2010
  */
 /* Compiler settings for .\SpiraTestCompletePlugIn.idl:
     Oicf, W1, Zp8, env=Win32 (32b run)
